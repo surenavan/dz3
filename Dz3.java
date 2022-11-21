b9c53dd7d741b3f37ee020b3a0a084bfe2653eb3
surenavan <109416683+surenavan@users.noreply.github.com>
2022-11-21 11:24:57 +0300

    Update Dz3.java

--- Dz3.java
+++ Dz3.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 
 public class Dz3 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите любое число x:"); int x = scanner.nextInt();
         int y = 3;
         double i = Math.pow(x, y);
-        System.out.println(i % 10);
+        double result = i / 10;
+        System.out.println(result % 7);
     }
 }
-
-
